d973fc5db0557245f35b03422d97ee055606fd02
Sarthak Agrawal <agrawal.sarthak2599@gmail.com>
2022-01-10 21:23:56 +0530

    learn the cleanup functionality of useEffect hook which work like componentWillUnmount

--- src/App.js
+++ src/App.js
@@ -9,15 +9,31 @@ function App(props) {
     // url for fetching the data as json object
     const url = `https://jsonplaceholder.typicode.com/posts?userId=${userId}`;
     // fetching the data
+    // componentDidMount
     fetch(url)
       .then ((response) => response.json()) // fetch returns a promise which we transform response in json format
       .then((data) => {
         console.log('DATA', data);
         // setData (data) cannot be called without passing a argument to useEffect as it will run till infinite as it is re-rendered every time when there is change in state 
+        // componentDidUpdate
         setData(data); 
       });
   // }, [] ); if we want useEffect to run single time in initiation
   },[userId] ); // Now it will only re-render when the userId is changed
+
+  // Using useEffect as a side-effect to learn about cleanup
+  useEffect(() => {
+    document.addEventListener('mousemove', onMouseMove);
+
+    // Provide a cleanup to remove the event listener after the data is fetched like componentWillUnmount
+    return ( () => {
+      document.removeEventListener('mousemove', onMouseMove);
+    })
+  });
+
+  function onMouseMove(event) {
+    console.log(event.clientX);
+  }
 
   return (
     <div className='App' style= {{ paddingLeft: 20}}>
